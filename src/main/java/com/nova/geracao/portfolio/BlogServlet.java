3d52b29a0be9b73fe460832e7935c6a2b6c036ef
Ismael Sarmento <ismael.sarmento.jr@gmail.com>
2015-10-22 21:15:00 -0300

    first commit

---
+++ src/main/java/com/nova/geracao/portfolio/BlogServlet.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright 2014-2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+//[START all]
+package com.nova.geracao.portfolio;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class BlogServlet extends HttpServlet {
+
+	private static final long serialVersionUID = -135873361334621489L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		RequestDispatcher dispatcher = req.getRequestDispatcher("html/blog/index.html");
+		dispatcher.forward(req, resp);
+	}
+ 
+}
